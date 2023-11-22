a2ede31227be45d5b036b3c82aae121124f29f42
Alexey Solovyov <plotinka@lyoha.info>
2023-11-22 20:40:04 +0400

    fix url

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://lyohaplotinka.github.io/rattus-orm/',
+  url: 'https://lyohaplotinka.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/rattus-orm/',
