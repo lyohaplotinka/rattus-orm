fde421f39ef474b647aef6fe5d62819505d86583
Alexey Solovyov <plotinka@lyoha.info>
2023-11-24 20:48:57 +0400

    pinia-bindings: remove extra method from database

--- packages/core/src/database/database.ts
+++ packages/core/src/database/database.ts
@@ -103,10 +103,6 @@ export class Database {
     return this.models[name] as M
   }
 
-  public getEntityNames(): string[] {
-    return Object.keys(this.models)
-  }
-
   /**
    * Get schema by the specified entity name.
    */
