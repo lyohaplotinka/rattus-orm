2c81b6125e3f389fce72c82f158a200e9a3cd715
Alexey Solovyov <plotinka@lyoha.info>
2024-01-18 20:19:53 +0400

    custom-repositories-management: ability to register custom repository

---
+++ packages/core/src/repository/repository-manager.ts
@@ -0,0 +1,30 @@
+import type { Constructor } from '@rattus-orm/utils/sharedTypes'
+
+import type { Database } from '@/database/database'
+import type { Model } from '@/model/Model'
+
+import { Repository } from './repository'
+
+export class RepositoryManager {
+  protected readonly repoConstructors = new Map<string, Constructor<Repository<any>>>([['*', Repository]])
+
+  public addRepositoryConstructor<T extends Repository>(
+    repoCtor: Constructor<T, ConstructorParameters<typeof Repository>>,
+    database: Database,
+  ) {
+    const repoInstance = new repoCtor(database)
+    if (!repoInstance.use) {
+      throw new Error('[Rattus ORM] custom repositories should have public "use" property with related model')
+    }
+    const entity = repoInstance.use.entity
+    this.repoConstructors.set(entity, repoCtor)
+  }
+
+  public getRepositoryCtorForModel<T extends Constructor<Repository>, M extends typeof Model>(model: M): T {
+    if (this.repoConstructors.has(model.entity)) {
+      return this.repoConstructors.get(model.entity) as T
+    }
+
+    return this.repoConstructors.get('*') as T
+  }
+}
