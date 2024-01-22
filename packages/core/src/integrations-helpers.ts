9e48b57a3b17374e0bee41de8eb9385b97245558
Alexey Solovyov <plotinka@lyoha.info>
2024-01-22 20:08:46 +0400

    refactor-use-repository: core helper for integrations

---
+++ packages/core/src/integrations-helpers.ts
@@ -0,0 +1,60 @@
+import type { RattusContext } from './context/rattus-context'
+import type { Model } from './model/Model'
+import type { Repository } from './repository/repository'
+
+const useRepositorySkippedKeys = ['database', 'use', 'model', 'constructor'] as const
+type UseRepositorySkippedKeys = (typeof useRepositorySkippedKeys)[number]
+const isSkippedKey = (value: unknown): value is UseRepositorySkippedKeys => {
+  return typeof value === 'string' && useRepositorySkippedKeys.includes(value as UseRepositorySkippedKeys)
+}
+
+export type ContextRetriever = () => RattusContext
+export type UseRepository<R extends Repository<InstanceType<M>>, M extends typeof Model = typeof Model> = Omit<
+  R,
+  UseRepositorySkippedKeys
+>
+
+function getAllKeys<T extends Record<any, any>>(obj: T): Array<keyof T> {
+  const keys = new Set<keyof T>()
+
+  do {
+    Object.getOwnPropertyNames(obj).forEach((key) => keys.add(key))
+    Object.getOwnPropertySymbols(obj).forEach((sym) => keys.add(sym))
+    obj = Object.getPrototypeOf(obj)
+  } while (obj && obj !== Object.prototype)
+
+  return Array.from(keys)
+}
+
+export function useRepositoryForDynamicContext<
+  R extends Repository<InstanceType<M>>,
+  M extends typeof Model = typeof Model,
+>(contextRetriever: ContextRetriever, model: M): UseRepository<R> {
+  const context = contextRetriever()
+  const repo = context.$repo<R, M>(model)
+  const allRepoKeys = getAllKeys(repo)
+  const result = {} as UseRepository<R>
+
+  for (const key of allRepoKeys) {
+    if (isSkippedKey(key)) {
+      continue
+    }
+    const repoValue = repo[key]
+
+    if (typeof repoValue === 'function') {
+      result[key as string] = repoValue.bind(repo)
+      continue
+    }
+
+    Object.defineProperty(repoValue, key, {
+      get(): R[typeof key] {
+        return repo[key]
+      },
+      set(v: any) {
+        repo[key] = v
+      },
+    })
+  }
+
+  return result
+}
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -5,6 +5,7 @@ export default createTsupConfig(
     'rattus-orm-core': './src/index.ts',
     'object-data-provider': './src/object-data-provider.ts',
     'rattus-context': './src/rattus-context.ts',
+    'integrations-helpers': './src/integrations-helpers.ts',
   },
   {
     splitting: true,
