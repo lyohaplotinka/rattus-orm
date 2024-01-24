feca7b8e77dbc83fa1b9374c8cb2cc34047cd945
Alexey Solovyov <plotinka@lyoha.info>
2024-01-24 20:05:05 +0400

    refactor-use-repository: update signatures for react-signals

--- packages/react-signals/src/hooks/useRepository.ts
+++ packages/react-signals/src/hooks/useRepository.ts
@@ -1,19 +1,11 @@
-import type { Model } from '@rattus-orm/core'
-import type { Repository } from '@rattus-orm/core'
-import { pickFromClass } from '@rattus-orm/utils/pickFromClass'
+import type { Model, Repository } from '@rattus-orm/core'
+import type { UseRepository } from '@rattus-orm/core/integrations-helpers'
+import { useRepositoryForDynamicContext } from '@rattus-orm/core/integrations-helpers'
 
-import type { PickedRepository, RepositoryCustomKeys } from './types'
-import { pullRepositoryKeys } from './types'
 import { useRattusContext } from './useRattusContext'
 
-export function useRepository<
-  T extends typeof Model,
-  CK extends RepositoryCustomKeys<R>,
-  R extends Repository<InstanceType<T>> = Repository<InstanceType<T>>,
->(model: T, pullCustomKeys: CK[] = []): PickedRepository<T, R, CK> {
-  const repo = useRattusContext().$repo(model) as R
-
-  const combinedKeys = Array.from(new Set([...pullRepositoryKeys, ...pullCustomKeys]))
-
-  return pickFromClass(repo, combinedKeys) as PickedRepository<T, R, CK>
+export function useRepository<R extends Repository<InstanceType<M>>, M extends typeof Model = typeof Model>(
+  model: M,
+): UseRepository<R, M> {
+  return useRepositoryForDynamicContext(useRattusContext, model)
 }
