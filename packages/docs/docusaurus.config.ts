7d203af113c7ef7f79221285c1ffcea4075c9dd4
Alexey Solovyov <plotinka@lyoha.info>
2024-01-29 00:01:23 +0400

    update docs baseUrl

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://lyohaplotinka.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rattus-orm/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   trailingSlash: false,
