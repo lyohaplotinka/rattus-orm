733311d65a423131cf2ff5c684966d8736520a70
Alexey Solovyov <plotinka@lyoha.info>
2024-02-03 15:24:57 +0400

    react-mobx: data provider and mobx-store class

---
+++ packages/react-mobx/src/mobx/mobx-store.ts
@@ -0,0 +1,49 @@
+import type { Elements, State } from '@rattus-orm/core'
+import { action, computed, makeObservable, observable } from 'mobx'
+
+export default class MobxStore {
+  protected state: State = { data: {} }
+
+  constructor(initialState?: State) {
+    makeObservable<this, 'state'>(this, {
+      state: observable,
+      save: action,
+      flush: action,
+      fresh: action,
+      destroy: action,
+      data: computed,
+    })
+    this.state = initialState ?? { data: {} }
+  }
+
+  public get data() {
+    return this.state
+  }
+
+  public destroy(ids: string[]) {
+    ids.forEach((id) => {
+      delete this.state.data[id]
+    })
+  }
+
+  public save(records: Elements) {
+    this.state = {
+      data: {
+        ...this.state,
+        ...records,
+      },
+    }
+  }
+
+  public flush() {
+    this.state = {
+      data: {},
+    }
+  }
+
+  public fresh(records: Elements) {
+    this.state = {
+      data: records,
+    }
+  }
+}
