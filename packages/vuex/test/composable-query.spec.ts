c2a11a9b4fca04f11070e3313631d3aa98d23122
Alexey Solovyov <plotinka@lyoha.info>
2024-02-20 14:21:11 +0400

    tests-refactor: vuex - use vueTestUtils

--- packages/vuex/test/composable-query.spec.ts
+++ packages/vuex/test/composable-query.spec.ts
@@ -1,45 +1,21 @@
-import { beforeEach, describe, expect } from 'vitest'
-import { createStore, Store } from 'vuex'
+import { describe, expect } from 'vitest'
+import { createStore } from 'vuex'
 import { installRattusORM } from '../src'
-import { mount } from '@vue/test-utils'
 import { useRepository } from '../src'
 import { isComputed } from '@rattus-orm/core/utils/vueComposableUtils'
 import { TestUser } from '@rattus-orm/core/utils/testUtils'
+import { renderHookWithContext } from '@rattus-orm/core/utils/vueTestUtils'
 
 describe('composable-query', () => {
-  let store: Store<any>
-
-  const withSetup = <T extends () => any>(hook: T) => {
-    let result: ReturnType<T>
-
-    mount(
-      {
-        template: '<div />',
-        setup() {
-          result = hook()
-        },
-      },
-      { global: { plugins: [store] } },
-    )
-
-    // @ts-ignore
-    return result as ReturnType<T>
-  }
-
-  beforeEach(() => {
-    store = createStore({
-      plugins: [installRattusORM()],
-    })
-
-    store.$rattusContext.$database.getRepository(TestUser).insert([
-      { id: '1', age: 22 },
-      { id: '2', age: 33 },
-      { id: '3', age: 44 },
-    ])
-  })
-
   it('withQuery returns computed property', () => {
-    const repo = withSetup(() => useRepository(TestUser))
+    const repo = renderHookWithContext({
+      hook: () => useRepository(TestUser),
+      plugins: [
+        createStore({
+          plugins: [installRattusORM()],
+        }),
+      ],
+    })
     const result = repo.withQuery((query) => query.where('age', (v: number) => v > 25))
     expect(isComputed(result)).toEqual(true)
   })
--- packages/vuex/test/composable.spec.ts
+++ packages/vuex/test/composable.spec.ts
@@ -1,93 +1,39 @@
-import { beforeEach, describe, expect, vi } from 'vitest'
-import { Component, ComponentOptions, computed, nextTick } from 'vue'
-import { mount } from '@vue/test-utils'
-import { Model, Num, Repository, Attr, Database } from '@rattus-orm/core'
-import { createStore, Store } from 'vuex'
+import { describe, expect } from 'vitest'
+import { computed, nextTick } from 'vue'
+import { Repository, Database } from '@rattus-orm/core'
+import { createStore } from 'vuex'
 import { installRattusORM, useRepository } from '../src'
 import { pullRepositoryGettersKeys, pullRepositoryKeys } from '@rattus-orm/core/utils/integrationsHelpers'
 import { useRattusContext } from '../src'
 import { RattusContext } from '@rattus-orm/core/utils/rattus-context'
-
-class User extends Model {
-  static entity = 'users'
-
-  @Attr()
-  public id: string
-
-  @Num(0)
-  public age: number
-}
-
-const componentTemplate: Component = {
-  template: `<div>Age: {{ age }}</div>`,
-}
-
-const attachSetup = (setup: ComponentOptions['setup']): Component => ({
-  ...componentTemplate,
-  setup,
-})
+import { renderHookWithContext, renderWithContext } from '@rattus-orm/core/utils/vueTestUtils'
+import { createBindSpy, TestUser } from '@rattus-orm/core/utils/testUtils'
 
 describe('composable: vuex', () => {
-  let store: Store<any>
-
-  beforeEach(() => {
-    store = createStore({
-      plugins: [installRattusORM()],
-    })
-
-    store.$rattusContext.$database.getRepository(User).insert([{ id: '1', age: 23 }])
-  })
-
-  const mountSetup = (setup: ComponentOptions['setup']) => {
-    const component = attachSetup(setup)
-
-    return mount(component, { global: { plugins: [store] } })
-  }
-
-  const withSetup = <T extends () => any>(hook: T) => {
-    let result: ReturnType<T>
-
-    mount(
-      {
-        template: '<div />',
-        setup() {
-          result = hook()
-        },
-      },
-      { global: { plugins: [store] } },
-    )
-
-    // @ts-ignore
-    return result as ReturnType<T>
-  }
-
   it('has correct context', () => {
-    const result = withSetup(() => useRattusContext())
+    const result = renderHookWithContext({
+      hook: useRattusContext,
+      plugins: [
+        createStore({
+          plugins: [installRattusORM()],
+        }),
+      ],
+    })
 
     expect(result).toBeInstanceOf(RattusContext)
     expect(result.$database).toBeInstanceOf(Database)
   })
 
   describe('useRepository returns correctly bound methods', () => {
-    store = createStore({
-      plugins: [installRattusORM()],
-    })
+    using _ = createBindSpy()
 
-    const mocked = vi.spyOn(Function.prototype, 'bind').mockImplementation(function (
-      this: any,
-      thisArg: any,
-      ...args: any[]
-    ) {
-      const func = this
-      const boundFunction = function (...newArgs: any[]): any {
-        return func.apply(thisArg, args.concat(newArgs))
-      }
-      boundFunction.boundTo = thisArg
-      return boundFunction
-    })
-
-    const result = withSetup(() => {
-      return useRepository(User)
+    const result = renderHookWithContext({
+      hook: () => useRepository(TestUser),
+      plugins: [
+        createStore({
+          plugins: [installRattusORM()],
+        }),
+      ],
     })
 
     it.each(pullRepositoryKeys)('%s has correct context', (methodName) => {
@@ -95,12 +41,17 @@ describe('composable: vuex', () => {
         expect((result[methodName] as any).boundTo).toBeInstanceOf(Repository)
       }
     })
-
-    mocked.mockRestore()
   })
 
   it('useRepository: methods are not ruined', () => {
-    const { insert, fresh, destroy, find, save, all, flush } = withSetup(() => useRepository(User))
+    const { insert, fresh, destroy, find, save, all, flush } = renderHookWithContext({
+      hook: () => useRepository(TestUser),
+      plugins: [
+        createStore({
+          plugins: [installRattusORM()],
+        }),
+      ],
+    })
     expect(() => insert({ id: '2', age: 22 })).not.toThrowError()
     expect(() => fresh([{ id: '1', age: 11 }])).not.toThrowError()
     expect(() => destroy('1')).not.toThrowError()
@@ -111,17 +62,27 @@ describe('composable: vuex', () => {
   })
 
   it('useRepository: returns reactive data', async () => {
-    const repo = store.$rattusContext.$database.getRepository(User)
-    const wrapper = mountSetup(() => {
-      const { find } = useRepository(User)
-      const user = find('1')
+    const wrapper = renderWithContext({
+      setup() {
+        const { find, insert } = useRepository(TestUser)
+        insert([{ id: '1', age: 23 }])
+        const user = find('1')
 
-      return {
-        age: computed(() => user.value?.age),
-      }
+        return {
+          age: computed(() => user.value?.age),
+        }
+      },
+      plugins: [
+        createStore({
+          plugins: [installRattusORM()],
+        }),
+      ],
     })
-    expect(wrapper.text()).toStrictEqual('Age: 23')
 
+    const rattusContext = wrapper.getCurrentComponent().appContext.config.globalProperties.$store.$rattusContext
+    const repo = rattusContext.$repo(TestUser)
+
+    expect(wrapper.text()).toStrictEqual('Age: 23')
     repo.query().update({ id: '1', age: 25 })
     await nextTick()
     expect(wrapper.text()).toStrictEqual('Age: 25')
