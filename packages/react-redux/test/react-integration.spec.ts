22ea1a35c3e5c8e33afaa9a3d0ca81df80fa1a66
Alexey Solovyov <plotinka@lyoha.info>
2024-02-20 11:55:10 +0400

    tests-refactor: common test for react integrations - using in signals

--- packages/react-redux/test/react-integration.spec.ts
+++ packages/react-redux/test/react-integration.spec.ts
@@ -1,6 +1,5 @@
 import { RattusProvider, ReactReduxDataProvider, useRattusContext, useRepository } from '../src'
 import { createStore } from 'redux'
-import '@testing-library/jest-dom/vitest'
 import { createReactIntegrationTest } from '@rattus-orm/core/utils/reactTestUtils'
 
 createReactIntegrationTest({
