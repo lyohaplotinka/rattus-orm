5bd09938f52da2bc9142e33e5ca41eb1e2d4aca8
Alexey Solovyov <plotinka@lyoha.info>
2024-06-09 01:51:15 +0400

    createDatabase function

--- packages/core/src/database/types.ts
+++ packages/core/src/database/types.ts
@@ -1,3 +1,11 @@
+import type { DataProvider } from '@/data/types'
 import type { Database } from '@/database/database'
+import type { MakeFieldsOptional } from '@/types'
 
 export type DatabasePlugin = (database: Database) => void
+export type CreateDatabaseParameters = {
+  connection: string
+  plugins: DatabasePlugin[]
+  dataProvider: DataProvider
+}
+export type CreateDatabaseFunctionParameters = MakeFieldsOptional<CreateDatabaseParameters, 'plugins' | 'connection'>
