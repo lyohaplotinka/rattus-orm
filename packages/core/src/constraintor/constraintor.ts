e35da6190418a4ff2a2be513a4c253d4968e6d18
Alexey Solovyov <plotinka@lyoha.info>
2024-06-12 23:45:16 +0400

    ⚙️ Refactor: query and constraintor are split (#41)
    * constraintor and query interface
    
    * update dependencies

---
+++ packages/core/src/constraintor/constraintor.ts
@@ -0,0 +1,71 @@
+import { Relation } from '@/model/attributes/relations/relation'
+import type { Model } from '@/model/Model'
+import type {
+  EagerLoad,
+  EagerLoadConstraint,
+  Order,
+  OrderBy,
+  OrderDirection,
+  Where,
+  WherePrimaryClosure,
+  WhereSecondaryClosure,
+} from '@/query/types'
+
+export class Constraintor<M extends Model> {
+  constructor(
+    protected readonly model: M,
+    protected eagerLoad: EagerLoad = {},
+    protected skip: number = 0,
+    protected take: number | null = null,
+    protected orders: Order[] = [],
+    protected wheres: Where[] = [],
+  ) {}
+
+  public clone(): Constraintor<M> {
+    return new Constraintor(this.model, this.eagerLoad, this.skip, this.take, this.orders, this.wheres)
+  }
+
+  public where(field: WherePrimaryClosure | string, value?: WhereSecondaryClosure | any) {
+    this.wheres.push({ field, value, boolean: 'and' })
+  }
+
+  public whereIn(field: string, values: any[]) {
+    this.wheres.push({ field, value: values, boolean: 'and' })
+  }
+
+  public whereId(ids: string | number | (string | number)[]) {
+    return this.where(this.model.$getKeyName() as any, ids)
+  }
+
+  public orWhere(field: WherePrimaryClosure | string, value?: WhereSecondaryClosure | any) {
+    this.wheres.push({ field, value, boolean: 'or' })
+  }
+
+  public orderBy(field: OrderBy, direction: OrderDirection = 'asc') {
+    this.orders.push({ field, direction })
+  }
+
+  public limit(value: number) {
+    this.take = value
+  }
+
+  public offset(value: number) {
+    this.skip = value
+  }
+
+  public with(name: string, callback: EagerLoadConstraint = () => {}) {
+    this.eagerLoad[name] = callback
+  }
+
+  public withAll(callback: EagerLoadConstraint = () => {}) {
+    for (const [name, value] of Object.entries(this.model.$fields())) {
+      value instanceof Relation && this.with(name, callback)
+    }
+  }
+
+  public withAllRecursive(depth: number = 3) {
+    this.withAll((query) => {
+      depth > 0 && query.withAllRecursive(depth - 1)
+    })
+  }
+}
--- packages/core/src/query/types.ts
+++ packages/core/src/query/types.ts
@@ -1,3 +1,6 @@
+import type { Collection, Element, Elements, Item } from '@/data/types'
+import type { Model } from '@/model/Model'
+
 import type { Query } from './query'
 
 export interface Where {
@@ -29,3 +32,48 @@ export interface EagerLoad {
 }
 
 export type EagerLoadConstraint = (query: Query) => void
+
+export interface QueryInterface<
+  M extends Model,
+  InsertionType = M,
+  SingleItemType = Item<M>,
+  MultiItemType = Collection<M>,
+> {
+  find: {
+    (id: string | number): SingleItemType
+    (ids: (string | number)[]): MultiItemType
+  }
+  revive: {
+    (schema: Element[]): MultiItemType
+    (schema: Element): SingleItemType
+  }
+  save: {
+    (records: Element[]): InsertionType[]
+    (record: Element): InsertionType
+  }
+  insert: {
+    (records: Element[]): MultiItemType
+    (record: Element): SingleItemType
+  }
+  fresh: {
+    (records: Element[]): MultiItemType
+    (record: Element): SingleItemType
+  }
+  destroy: {
+    (ids: (string | number)[]): MultiItemType
+    (id: string | number): SingleItemType
+  }
+  new: () => InsertionType
+  all(): MultiItemType
+  get(): MultiItemType
+  first(): SingleItemType
+  findIn(ids: (string | number)[]): MultiItemType
+  select(): MultiItemType
+  load(models: Collection<M>): MultiItemType
+  reviveOne(schema: Element): SingleItemType
+  reviveMany(schema: Element[]): MultiItemType
+  saveElements(elements: Elements): unknown
+  update(record: Element): MultiItemType
+  delete(): InsertionType[]
+  flush(): MultiItemType
+}
