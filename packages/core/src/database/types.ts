88062efa739dcd6bd6a79ba36349ce7e0c5fed97
Alexey Solovyov <plotinka@lyoha.info>
2024-09-08 17:46:19 +0400

    do not use type-fest in packages

--- packages/core/src/database/types.ts
+++ packages/core/src/database/types.ts
@@ -1,9 +1,7 @@
-import type { Constructor } from 'type-fest'
-
 import type { DataProvider } from '@/data/types'
 import type { Database } from '@/database/database'
 import type { Repository } from '@/repository/repository'
-import type { MakeRequired } from '@/types'
+import type { Constructor, MakeRequired } from '@/types'
 
 export type DatabasePlugin = (database: Database) => void
 export type CreateDatabaseParameters = {
