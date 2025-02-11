9ded8ce148525b5ba545c26aa40a831577c96ef9
Alexey Solovyov <plotinka@lyoha.info>
2025-02-11 20:18:17 +0100

    read changelog without import statement

--- scripts/src/auto-changelogger/index.ts
+++ scripts/src/auto-changelogger/index.ts
@@ -1,4 +1,5 @@
 import { writeFileSync } from 'node:fs'
+import { readFile } from 'node:fs/promises'
 
 import { format } from 'date-fns/format'
 import { uniq } from 'lodash-es'
@@ -10,15 +11,18 @@ import type { ChangelogElement } from './types'
 
 const CHANGELOG_JSON_PATH = `${MONOREPO_ROOT_DIR}/.yarn/pkgmeta/changelog.json`
 
+async function getChangelog(): Promise<Record<string, ChangelogElement[]>> {
+  const content = await readFile(CHANGELOG_JSON_PATH, 'utf8')
+  return JSON.parse(content)
+}
+
 export async function updateChangelog(forPackage: string) {
   const changes = await getChangelogElementsForPackage(forPackage)
   if (!changes) {
     return
   }
   const date = format(new Date(), 'yyyy/MM/dd')
-  const { default: changelogJson } = (await import(CHANGELOG_JSON_PATH, { assert: { type: 'json' } })) as {
-    default: Record<string, ChangelogElement[]>
-  }
+  const changelogJson = await getChangelog()
 
   if (!(date in changelogJson)) {
     changelogJson[date] = []
